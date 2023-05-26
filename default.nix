e9bec5742989ddda47f585a392d2ec6cc0ba8677
74k1 <taki@eva02>
2023-05-26 19:04:49 +0200

    yee

--- default.nix
+++ default.nix
@@ -1,17 +1,18 @@
-{ stdenv, gtk-engine-murrine }:
+let
+  pkgs = import <nixpkgs> {};
+in
+  pkgs.stdenv.mkDerivation rec {
+    pname = "Ukiyo";
+    version = "0.1";
 
-stdenv.mkDerivation rec {
-  pname = "Ukiyo";
-  version = "0.1";
+    src = ./.;  # This says "the source is the current directory"
 
-  src = ./.;
+    buildInputs = [ pkgs.gtk-engine-murrine ];
 
-  buildInputs = [ gtk-engine-murrine ];
-
-  installPhase = ''
-    mkdir -p $out/share/themes/Ukiyo
-    cp -r usr/share/themes/Ukiyo/* $out/share/themes/Ukiyo
-    mkdir -p $out/share/icons/Ukiyo
-    cp -r usr/share/icons/Ukiyo/* $out/share/icons/Ukiyo
-  '';
-}
+    installPhase = ''
+      mkdir -p $out/share/themes/Ukiyo
+      cp -r usr/share/themes/Ukiyo/* $out/share/themes/Ukiyo
+      mkdir -p $out/share/icons/Ukiyo
+      cp -r usr/share/icons/Ukiyo/* $out/share/icons/Ukiyo
+    '';
+  }
